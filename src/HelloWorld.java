e320471122468344277a504b8041fe8b457044a8
samridhi <samridhig750@gmail.com>
2024-09-25 19:45:03 +0530

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //type name = new type
+
+        Greeting greeting = new Greeting();
+
+          greeting.greet();
+
+    }
+
+}
+
+
+
