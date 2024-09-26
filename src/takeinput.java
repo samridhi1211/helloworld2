01e020f9d08d12866982b8b80f6c3dff75155153
samridhi <samridhig750@gmail.com>
2024-09-26 23:01:54 +0530

    first commit

---
+++ src/takeinput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class takeinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter name");
+
+
+        String name = sc.next();
+        System.out.println("name:"+name);
+
+        System.out.println("enter lucky number");
+        int num_1 =sc.nextInt();
+
+        System.out.println("lucky no."+num_1);
+    }
+}
