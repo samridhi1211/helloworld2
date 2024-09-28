f2391da1d460d99975f5afad5fe18b730417c630
samridhi <samridhig750@gmail.com>
2024-09-28 23:17:07 +0530

    first commit

---
+++ src/IntegerInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class IntegerInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("input a float value");
+        float float_num = sc.nextFloat();
+
+        System.out.println("input is :"+ float_num);
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,13 +1,11 @@
 public class test {
     public static void main(String[] args) {
-        int a, b;
-        a = 10;
-        b = (a == 1) ? 1 : 0;
-        System.out.println("value of b:" + b);
 
-        b = (a == 10) ? 1: 0;
+        String  name = "shail";
 
-        System.out.println("value of a:" + b);
+        boolean result = name instanceof String;
+        System.out.println(result);
+
 
     }
 }
