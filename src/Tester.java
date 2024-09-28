529b3291b08fb2149e51f2e4200500b82c80cf2b
samridhi <samridhig750@gmail.com>
2024-09-28 22:39:20 +0530

    first commit

--- src/Tester.java
+++
@@ -1,8 +0,0 @@
-
-public class Tester {
-    public static void main(String[] args) {
-
-        char unicodechar = '\u00041';
-        System.out.println("stored unicode character" + unicodechar);
-    }
-}
---
+++ src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        int a, b;
+        a = 10;
+        b = (a == 1) ? 20 : 30;
+        System.out.println("value of b:" + b);
+
+        b = (a == 10) ? 40 : 20;
+
+        System.out.println("value of a:" + a);
+
+    }
+}
