77cf9d6fb73b319019961d52be93fc473cbc19a6
samridhi <samridhig750@gmail.com>
2024-09-28 22:07:34 +0530

    first commit

--- src/Tester.java
+++ src/Tester.java
@@ -3,7 +3,7 @@ public class Tester {
     public static void main(String[] args) {
         int num1 = 5084;
         double  num2 = 22.33;
-        int sum = num1 + num2;
+        double sum = num1 + num2;
 
         System.out.println("sum of " +num1+" and "+ num2 +" is" + sum);
     }
