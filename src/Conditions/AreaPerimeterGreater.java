062454f6734b447398de392955189cabee00cf32
samridhi <samridhig750@gmail.com>
2024-09-30 22:28:17 +0530

    first commit

--- src/Conditions/AreaPerimeterGreater.java
+++ src/Conditions/AreaPerimeterGreater.java
@@ -1,4 +1,30 @@
 package Conditions;
+import java.util.Scanner;
 
 public class AreaPerimeterGreater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean KeepRunning = true;
+
+        while (KeepRunning){
+            try{
+                System.out.println("enter a length :" );
+                int l = sc.nextInt();
+                System.out.println("enter a breadth :" );
+                int b = sc.nextInt();
+                int perimeter = 2*(l+b);
+                int area = l*b;
+                if(area > perimeter){
+                    System.out.println("area  is greater than perimeter");
+                }else if(perimeter == area) {
+                    System.out.println(" both perimeter and area are equal  ");
+                }else {
+                    System.out.println("perimeter is greater than area");
+                }
+            }catch(Exception ex){
+                KeepRunning = false;
+                System.out.println("exit program");
+            }
+        }
+    }
 }
