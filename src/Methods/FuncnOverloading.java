421ec40cb2305a9d832533035612db054eaba5c9
samridhi <samridhig750@gmail.com>
2024-10-05 13:16:57 +0530

    first commit

--- src/Methods/FuncnOverloading.java
+++ src/Methods/FuncnOverloading.java
@@ -1,4 +1,17 @@
 package Methods;
-
+import java.util.Arrays;
 public class FuncnOverloading {
+    public static void main(String[] args) {
+
+      int ans =  sum(2,4);
+        System.out.println(ans);
+    }
+
+    static int sum(int a, int b){
+        return a+b;
+    }
+    static void fun(int a){
+        System.out.println("first one" );
+        System.out.println(a);
+    }
 }
--- src/Methods/VarArguments.java
+++ src/Methods/VarArguments.java
@@ -2,10 +2,18 @@ package Methods;
 import java.util.Arrays;
 public class VarArguments {
     public static void main(String[] args) {
-        multiple (2,3"kunal", "shiv", "reem");
+//        multiple (2,3, "shiv", "reem");
+        demo("de","we","to");
+
     }
-    static void multiple( int a , int b, String ...v) {
+    static void demo(int ...v) {
         System.out.println(Arrays.toString(v));
     }
+    static void demo(String ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+//    static void multiple( int a , int b, String ...v) {
+//        System.out.println(Arrays.toString(v));
+    }
 
-}
+
