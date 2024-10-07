fb9b05393d4795f3e9f502107e3d485bfaeb14f2
samridhi <samridhig750@gmail.com>
2024-10-07 23:30:48 +0530

    first commit

--- src/data/array/FindMinimum.java
+++ src/data/array/FindMinimum.java
@@ -1,6 +1,30 @@
 package data.array;
-
+import java.util.Scanner;
 public class FindMinimum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("minimum numbers to be compared?");
+        int n =sc.nextInt();
+        while(n<=0){
+            System.out.println("invalid number");
+        }
+        int []numbers = new int[n];
+        while(n !=0){
+            n--;
+            System.out.println("enter numbers");
+            numbers[n] = sc.nextInt();
+        }
+        int min = numbers[0];
+        for(int i =0;i<numbers.length; i++){
+            if(min > numbers[i]){
+                min=numbers[i];
+            }
+        }
+        System.out.println("minimum number :" + min);
+
+
+    }
     // array of numbers
     // find the minimum 
 }
--- src/data/array/Main.java
+++ src/data/array/Main.java
@@ -5,18 +5,17 @@ import data.ll.LLNode;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = new int[]{1,2,3,4,5};
-        LLNode head = new LLNode();
+        LLNode  head = new LLNode();
         LLNode current = head;
         for(int i = 0; i < numbers.length;  i++){
             System.out.println(i+" - "+numbers[i]);
             current.value = numbers[i];
             current.next = new LLNode();
-            current = current.next;
-            if(LLNode head == ){
-
-                break;
-
+            if(i==numbers.length-1){
+                current.next = null;
             }
+            current = current.next;
+
 
         }
     }
