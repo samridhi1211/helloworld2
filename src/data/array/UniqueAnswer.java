b335e16808efe7795376602267592927e826be95
samridhi <samridhig750@gmail.com>
2024-11-02 12:42:21 +0530

    first commit

--- src/data/array/UniqueAnswer.java
+++ src/data/array/UniqueAnswer.java
@@ -1,4 +1,40 @@
 package data.array;
 
+import java.util.Scanner;
+
 public class UniqueAnswer {
-}
+    static int findUniq(int[] arr){
+        int n = arr.length;
+        for(int i=0; i<n ; i++){
+            for(int j = i+1; j< n; j++){
+                if(arr[i]== arr[j]){
+                    arr[i] = -1;
+                    arr[j] = -1;
+
+                }
+            }
+        }
+        int ans= -1;
+        for(int i= 0; i<n; i++){
+            if(arr[i] > 0) {
+                ans = arr[i];
+            }
+            }
+            return ans;
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner (System.in);
+            System.out.println("enter array size");
+            int n = sc.nextInt();
+            int []arr = new int[n];
+            System.out.println("enter "+n + "element :" );
+            for(int i = 0; i<n ; i++){
+                arr[i] = sc.nextInt();
+            }
+//            System.out.println("enter target sum");
+//            int target = sc.nextInt();
+
+            System.out.println("unique element :"+ findUniq(arr));
+        }
+    }
+
