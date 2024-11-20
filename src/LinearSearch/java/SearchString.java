81a5c0d81ea2d578925459e6aa44367237e36077
samridhi <samridhig750@gmail.com>
2024-11-20 11:52:22 +0530

    first commit

---
+++ src/LinearSearch/java/SearchString.java
@@ -0,0 +1,35 @@
+package LinearSearch.java;
+
+
+public class SearchString {
+    public static void main(String[] args) {
+        String name ="kunal";
+        char target = 'u';
+        System.out.println(search(name , target));
+    }
+
+    static boolean search2(String str, char target){
+        if(str.length() == 0) {
+            return false;
+        }
+
+      for(char ch : str.toCharArray()) {
+          if(str.length() == 0) {
+              return false;
+          }
+      }
+        return false;
+    }
+    static boolean search(String str, char target){
+        if(str.length() == 0) {
+            return false;
+        }
+
+        for(int i = 0; i< str.length() ; i++){
+            if(target == str.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
