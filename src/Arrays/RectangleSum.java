65bf5cbac5c8cb05b019e2e256ad181c9b8ac088
samridhi <samridhig750@gmail.com>
2024-12-30 12:55:43 +0530

    first commit

--- src/Arrays/RectangleSum.java
+++ src/Arrays/RectangleSum.java
@@ -14,6 +14,8 @@ public class RectangleSum {
         return sum;
     }
 
+    //calculate Row-Wise and Column-Wise SUM
+    //matrix[i][j] = sumRectangle( (0,0)  (i,j) )
     static void findPrefixSumMatrix(int[][] matrix){
         int r = matrix.length;
         int c = matrix[0].length;
@@ -24,7 +26,15 @@ public class RectangleSum {
                 matrix[i][j] += matrix[i][j-1];
             }
         }
+
+        // traverse vertically to calculate column-wise Sum
+        for(int j=0 ; j < c ; j++){
+            for(int i=1; i < r ; i++){
+                matrix[i][j] += matrix[i-1][j];
+            }
+        }
     }
+
     static int findSum2(int[][] matrix, int l1, int r1 , int l2 , int r2) {    //l1 and l2 -> 1st row and 2nd row         r1 and r2 -> 1st col. and 2nd col.
         int sum =0;
         findPrefixSumMatrix(matrix);
@@ -39,7 +49,23 @@ public class RectangleSum {
         return sum;
     }
 
+    static int findSum3(int[][] matrix, int l1, int r1 , int l2 , int r2) {    //l1 and l2 -> 1st row and 2nd row         r1 and r2 -> 1st col. and 2nd col.
+        int sum = 0, ans = 0 , up = 0, left = 0, leftUp = 0;
+        findPrefixSumMatrix(matrix);
 
+        sum = matrix[l2][r2];
+        if(r1>= 1){
+            left = matrix[l2][r1-1];
+        }
+        if(l1 >= 1){                     // if condn mandatory as it not goes in negative
+            up = matrix[l1-1][r2];
+        }
+        if(l1 >= 1 &&  r1 >= 1){
+            leftUp = matrix[l1-1][r1-1];
+        }
+        ans = sum - up - left + leftUp;
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number of rows and coloumn");
@@ -61,7 +87,7 @@ public class RectangleSum {
         int r2 = sc.nextInt();
 
         System.out.println("rectangle Sum  " +  findSum(matrix, l1 , r1 ,l2, r2));
-        System.out.println("rectangle Sum  " +  findSum2(matrix, l1 , r1 ,l2, r2));
+        System.out.println("rectangle Sum  " +  findSum3(matrix, l1 , r1 ,l2, r2));
 
 
     }
